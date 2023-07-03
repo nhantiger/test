4560f66b5cb02516073246d1bb598fd7d93edfed
nhantiger <92725217+nhantiger@users.noreply.github.com>
2023-07-03 13:40:39 +0700

    create

--- src/App.js
+++ src/App.js
@@ -1,25 +1,156 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const data = {
+  reports: [
+    {
+      section: "Tasks",
+      lines: ["[To Do List Report(msptodo.pas)](/JOSListingReport/ToDoList)"],
+    },
+    {
+      section: "Sample Order",
+      lines: [
+        "[To Do List Report(msptodo.pas)](/JOSListingReport/ToDoList)",
+        "[Customer Quotation](/en/JOSListingReport/CustomerQuotation)",
+        "[Sample Order List (smpsmord.pas)](/JOSListingReport/SampleOrderList)",
+        "[Sample Inventory List (smpinvty.pas)](/JOSListingReport/SampleInventoryList)",
+        "[Sample Dispatch List (smpdispatch.pas)](/JOSListingReport/SampleDispatchList)",
+      ],
+    },
+    {
+      section: "Sales",
+      lines: [
+        "[Actual Shipping Report (sapacshp.pas)](/JOSListingReport/ActualShippingReport)",
+        "[Actual Shipping](/en/igarment/reports/actual_shipping)",
+        "[Close Order Analysis (sapcoa.pas)](/JOSListingReport/CloseOrderAnalysis)",
+        "[Debtors Age Analysis (SA-AgeAnalysis.nrs)](/JOSListingReport/DebtorsAgeAnalysis)",
+        "[Margin Report (sapmargin.pas)](/JOSListingReport/MarginReport)",
+        "[Monthly Sales and Gross Profit Report (sapmonthprofit.pas)](/JOSListingReport/MonthlySalesandGrossProfit)",
+        "[Order Cutting Shipped Compare Report (sapcut.pas)](/JOSListingReport/OrderCuttingShippedCompareReport)",
+        "[Production Cost Summary (sapprdcstsum.pas)](/JOSListingReport/ProductionCostSummary) ",
+        "[Production Progress (sapprdprogress.pas)](/JOSListingReport/ProductionProgress) ",
+        "[Sales Amendment Report (sapamendlist.pas)](/JOSListingReport/SalesAmendment)",
+        "[Sales Order Cross Analysis (sapcros.pas)](/JOSListingReport/SalesOrderCrossAnalysis)",
+        "[Sales Ranking Analysis (saprank.pas)](/JOSListingReport/SalesRankingAnalysis)",
+        "[Sales Trend Analysis](/en/JOSListingReport/TrendAnalysisofOrderQuantity)",
+        "[Sales and Profit Analysis](/en/JOSListingReport/MonthlySalesandGrossProfit)",
+        "[SalesAmendment](/en/JOSListingReport/SalesAmendment)",
+        "[SalesOrderCrossAnalysis](/en/JOSListingReport/SalesOrderCrossAnalysis)",
+        "[SalesRankingAnalysis](/en/JOSListingReport/SalesRankingAnalysis)",
+        "[SampleDispatchList](/en/JOSListingReport/SampleDispatchList)",
+        "[SampleInventoryList](/en/JOSListingReport/SampleInventoryList)",
+        "[SampleOrderList](/en/JOSListingReport/SampleOrderList)",
+        "[Shipment Schedule (Price) (sapshipa.pas)](/JOSListingReport/ShipmentSchedulePrice)",
+        "[Shipment Schedule (sapship.pas)](/JOSListingReport/ShipmentSchedule)",
+        "[Shipment Schedule Summary (sapsche.pas)](/JOSListingReport/ShipmentScheduleSummary)",
+        "[ShipmentSchedulePrice](/en/JOSListingReport/ShipmentSchedulePrice)",
+        "[ShipmentScheduleSummary](/en/JOSListingReport/ShipmentScheduleSummary)",
+        "[ShipmentSchedule](/en/JOSListingReport/ShipmentSchedule)",
+        "[Trend Analysis of Order Quantity (sa-ordertrendanalysis.nrs)](/JOSListingReport/TrendAnalysisofOrderQuantity)",
+      ],
+    },
+    {
+      section: "Progress",
+      lines: [
+        "[Colors Waiting for Approval](/en/JOSListingReport/unapprovedcolor)",
+        "[Daily Receiving](/en/JOSListingReport/DailyReceiving)",
+        "[Daily Trx](/en/JOSListingReport/DailyTrx)",
+        "[Debtors Age Analysis](/en/JOSListingReport/DebtorsAgeAnalysis)",
+      ],
+    },
+    {
+      section: "Material Requirements",
+      lines: [
+        "[Material Requirement By Job](/en/JOSListingReport/MatrReqByJob)",
+        "[Material Requirement By Material](/en/JOSListingReport/MatrReqByMatr)",
+        "[Material Requirement Status](/en/JOSListingReport/MaterialRequirementStatus)",
+        "[Material Requirement Status Summary](/en/JOSListingReport/MaterialRequirementStatusSummary)",
+      ],
+    },
+    {
+      section: "Material Purchasing",
+      lines: [
+        "[Daily Receiving Report (pupdlyrv.pas)](/JOSListingReport/DailyReceiving)",
+        "[Exceptional Receiving Report(pupexcp.pas)](/JOSListingReport/ExceptionalReceiving)",
+        "[Material Delivery Schedule Summary(pupssch.pas)](/JOSListingReport/MaterialDeliveryScheduleSummary)",
+        "[Material Delivery Schedule(pupsche.pas)](/JOSListingReport/MaterialDeliverySchedule)",
+        "[Material Purchasing Age Analysis](/en/JOSListingReport/POAgeAnalysis)",
+        "[Material Purchasing Cash Requirement Age Analysis (PO-AgeAnalysis.nrs)](/JOSListingReport/POAgeAnalysis)",
+        "[Material Purchasing Payment Schedule (PaymentSchedule.nrs)](/JOSListingReport/PaymentSchedule)",
+        "[Material Purchasing Payment Schedule Summary (PaymentScheduleSummary.nrs)](/JOSListingReport/PaymentScheduleSummary)",
+        "[Material Requirement Schedule(By Job)(MR-MatrReqByJob.nrs)](/JOSListingReport/MatrReqByJob)",
+        "[Material Requirement Schedule(By Material)(MR-MatrReqByMatr.nrs)](/JOSListingReport/MatrReqByMatr)",
+        "[Material Requirement Status Report(mrpreqa.pas)](/JOSListingReport/MaterialRequirementStatus)",
+        "[Material Requirement Status Summary (mrpreqasum.pas)](/JOSListingReport/MaterialRequirementStatusSummary)",
+        "[MaterialDeliveryScheduleSummary](/en/JOSListingReport/MaterialDeliveryScheduleSummary)",
+        "[MaterialDeliverySchedule](/en/JOSListingReport/MaterialDeliverySchedule)",
+        "[Purchase Order Status Amt](/en/JOSListingReport/POStatusAmt)",
+        "[Purchase Order Status Listing](/en/JOSListingReport/POStatus)",
+        "[Purchase Order Status(Amount)(pupstusa.pas)](/JOSListingReport/POStatusAmt)",
+        "[Purchase Order Status(pupstus.pas)](/JOSListingReport/POStatus)",
+        "[Supplier Invoice Receiving Transaction(pupinvc.pas)](/JOSListingReport/SuppIvcRcvtrx)",
+      ],
+    },
+  ],
+};
+
+const App = () => {
+  const [expandedSection, setExpandedSection] = useState("");
+
+  const sortLines = (lines) => {
+    return lines.sort((a, b) => {
+      const linkA = extractLink(a);
+      const linkB = extractLink(b);
+      return linkA.localeCompare(linkB);
+    });
+  };
+
+  const extractLink = (line) => {
+    const startIndex = line.lastIndexOf("(");
+    const endIndex = line.lastIndexOf(")");
+    return line.substring(startIndex + 1, endIndex).replace("/en", "");
+  };
+
+  const createLink = (line) => {
+    const startIndex = line.indexOf("[");
+    const endIndex = line.indexOf("]");
+    const title = line.substring(startIndex + 1, endIndex);
+    const link = extractLink(line);
+
+    return <a href={`https://igx.biz${link}`}>{title}</a>;
+  };
+
+  const toggleSection = (section) => {
+    setExpandedSection((prevSection) =>
+      prevSection === section ? "" : section
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      {data.reports.map((report, index) => (
+        <div
+          key={index}
+          className={`section ${
+            expandedSection === report.section ? "expanded" : ""
+          }`}
         >
-          Learn React
-        </a>
-      </header>
+          <span
+            className="section-title"
+            onClick={() => toggleSection(report.section)}
+          >
+            {report.section}
+          </span>
+          {expandedSection === report.section && (
+            <ol className="section-items">
+              {sortLines(report.lines).map((line, lineIndex) => (
+                <li key={lineIndex}>{createLink(line)}</li>
+              ))}
+            </ol>
+          )}
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
